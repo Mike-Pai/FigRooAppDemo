eced5980995a7f85b7a98b674b4d58bd65223d32
白謹瑜 <ye518612@gmail.com>
2024-12-08 21:03:34 +0800

    Initial Commit

---
+++ FigRooAppDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FigRooAppDemo
+//
+//  Created by 白謹瑜 on 2024/12/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FigRooAppDemo/FigRooAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigRooAppDemoApp.swift
+//  FigRooAppDemo
+//
+//  Created by 白謹瑜 on 2024/12/8.
+//
+
+import SwiftUI
+
+@main
+struct FigRooAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
